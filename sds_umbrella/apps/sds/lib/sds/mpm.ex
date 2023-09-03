ea3f2e26870660917723265377c59ec91e8ce367
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2023-09-03 19:37:18 -0400

    memory + tests

---
+++ sds_umbrella/apps/sds/lib/sds/mpm.ex
@@ -0,0 +1,82 @@
+defmodule Sds.Memory do
+  @moduledoc """
+  Provide the memory for the 940, with the specified number of pages
+  Each memory word is an integer between 0 and 16777215 inclusive.
+  Reads of unassigned memory return 2^32; it is up to the caller to handle properly
+  Illegal addresses will cause exceptions
+  The value written to memory must be within the given range. Out of range values
+  will cause exceptions
+
+  There are two interfaces
+  * read/write with a raw address in the range of @n_pages * @page_size
+  * read/write with a memory map and an address
+
+  There is also a method to delete a page for use when the OS releases a page
+  """
+  defstruct r_count: 0, w_count: 0, pmem: %{}
+
+  @n_pages 32
+  @n_virtual_pages 8
+  @page_size 2048
+
+  @doc """
+  return the memory size in words
+  """
+  def get_memory_size(), do: @n_pages * @page_size
+
+  @doc """
+  read a value given absolute addressing of the @n_pages memory. Keep track of # of reads.
+  """
+  def read(%__MODULE__{} = mem, address)
+      when is_integer(address) and address >= 0 and address < @n_pages * @page_size do
+    new_r_count = mem.r_count + 1
+    new_mem = %{mem | r_count: new_r_count}
+    content = Map.get(mem.pmem, address, 2 ** 32)
+    {new_mem, content}
+  end
+
+  @doc """
+  write a value to a given absolute address of the @n_pages memory. Track # of writes
+  16777216
+  """
+  def write(%__MODULE__{} = mem, address, content)
+      when is_integer(address) and address >= 0 and address < @n_pages * @page_size and
+             is_integer(content) and content >= 0 and content < 256 * 256 * 256 do
+    new_w_count = mem.w_count + 1
+    new_pmem = Map.put(mem.pmem, address, content)
+    %{mem | w_count: new_w_count, pmem: new_pmem}
+  end
+
+  @doc """
+  read a value given a memory map and an address.
+  The memory map is a tuple with 8 integers, representing the 8 pages of the SDS 940 hardware.
+  An unassigned page is represented by -1; legal page numbers are 0 <= page_number < @n_pages
+  The virtual address is in the range [0, 16383]
+
+  exceptions for out of range parameters will happen through guards in the called functions
+  """
+  def read_mapped(%__MODULE__{} = mem, v_address, map) do
+    absolute_address = get_absolute_address(v_address, map)
+    read(mem, absolute_address)
+  end
+
+  @doc """
+  write a value into memory at the given virtual address
+  """
+  def write_mapped(%__MODULE__{} = mem, v_address, map, content) do
+    absolute_address = get_absolute_address(v_address, map)
+    write(mem, absolute_address, content)
+  end
+
+  @doc """
+  translate a mapped virtual address into an absolute address
+  """
+  defp get_absolute_address(v_address, map)
+       when is_integer(v_address) and v_address >= 0 and v_address < @n_virtual_pages * @page_size and
+              is_tuple(map) and tuple_size(map) == @n_virtual_pages do
+    v_page = div(v_address, @page_size)
+    address_in_page = rem(v_address, @page_size)
+    p_page = elem(map, v_page)
+    (p_page * @page_size + address_in_page) # the return value
+  end
+end
---
+++ sds_umbrella/apps/sds/test/mpm_test.exs
@@ -0,0 +1,53 @@
+defmodule MemoryTest do
+  use ExUnit.Case
+  alias Sds.Memory
+  # doctest MemoryMemoryTest
+
+  test "initial memory" do
+    m = %Memory{}
+    assert m.r_count == 0
+    assert m.w_count == 0
+    assert m.pmem == %{}
+    assert Memory.get_memory_size == 65536
+  end
+
+  test "write memory" do
+    m = %Memory{}
+    m = Enum.reduce(0..31, m, fn page, mem -> Memory.write(mem, page*2048, page*2048) end)
+    assert m.r_count == 0
+    assert m.w_count == 32
+    assert Map.get(m.pmem, 16384) == 16384
+  end
+
+  test "read memory" do
+    m = %Memory{}
+    {m, val} = Memory.read(m, 0)
+    assert val == 2 ** 32
+    assert m.r_count == 1
+    assert m.w_count == 0
+    assert m.pmem == %{}
+  end
+
+  test "write and read" do
+    m = %Memory{}
+    m = Enum.reduce(0..2047, m, fn address, mem -> Memory.write(mem, address, address*2) end)
+    vals = Enum.map(0..2047, fn address -> ({_,v} = Memory.read(m, address); v) end)
+    assert length(vals) == 2048
+    assert Enum.max(vals) == 4094
+    assert Enum.min(vals) == 0
+    assert Enum.sum(vals) == div(2048 * 4094, 2)
+    assert {m.w_count, m.r_count} == {2048, 0}
+    m = Enum.reduce(0..2047, m, fn address, mem -> ({m1, _} = Memory.read(mem, address); m1) end)
+    assert {m.w_count, m.r_count} == {2048, 2048}
+  end
+
+  test "write sequential" do
+    m = %Memory{}
+    count = 65536
+    address_range = 0..count-1
+    m = Enum.reduce(address_range, m, fn address, m -> Memory.write(m, address, 2*address) end)
+    vals = Enum.map(address_range, fn address -> ({_,v} = Memory.read(m, address); v) end)
+    assert m.w_count == count
+    assert Enum.sum(vals) == div(count * 2 * (count-1), 2) # I know, it doesn't need the div if no "* 2"
+  end
+end
