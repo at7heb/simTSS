c74e3f9ea4ac5f8a7980c30e6206653de45c2159
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2023-09-09 12:35:24 -0400

    start machine impl.

--- sds_umbrella/apps/sds/lib/sds/machine.ex
+++ sds_umbrella/apps/sds/lib/sds/machine.ex
@@ -1,3 +1,7 @@
 defmodule Sds.Machine do
-
+  @moduledoc """
+  This is a simulated SDS 940 user process simulator.
+  It comprises a memory and a process queue.
+  It simulates @n_instructions and then switches to the next processes
+  """
 end
